bfe5f0b923fe6e0478fe420e0a8b4c59ec4cda08
Risolat <utkurova.risolat@mail.ru>
2020-04-18 22:49:05 +0500

    Updates

---
+++ precache-manifest.ddc21a69010970d8ff5caa9a41aa62a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adc1a4a90a71ff028d102c62b8e9af5b",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "716ca501864d3045e1c7",
+    "url": "/covid-19/static/css/main.d8182cc6.chunk.css"
+  },
+  {
+    "revision": "fa859a30065e7877c993",
+    "url": "/covid-19/static/js/2.0f58c988.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.0f58c988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "716ca501864d3045e1c7",
+    "url": "/covid-19/static/js/main.7c13d3c6.chunk.js"
+  },
+  {
+    "revision": "5947671d1c53a9002efe",
+    "url": "/covid-19/static/js/runtime-main.db8c32d2.js"
+  }
+]);
